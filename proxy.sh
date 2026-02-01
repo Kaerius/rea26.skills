7a8879b63f22bba8c52fd915503b68f2a2d37d30
Kaerius <kddkda@mail.ru>
2026-02-01 16:09:52 +0500

    Create proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#/etc/profile.d/proxy.sh
+export http_proxy="http://proxy.tech.skills:3128"
+export https_proxy="http://proxy.tech.skills:3128"
+
+export http_proxy="http:/10.113.38.138:3128"
+export https_proxy="http://10.113.38.138:3128"
+
+export no_proxy="localhost,127.0.0.1,.rea26.,192.168.1.,192.168.122."
